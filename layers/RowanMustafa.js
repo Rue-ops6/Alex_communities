e04df317d38cc912c8adc7797f3cf95576ac03df
Rue-ops6 <rowanmahrous01@gmail.com>
2025-05-28 06:46:46 +0300

    plzj

---
+++ layers/RowanMustafa.js
@@ -0,0 +1,10 @@
+var json_RowanMustafa = {
+
+"type": "FeatureCollection",
+"name": "RowanMustafa",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 6, "TheCreator": "The Creator: Rowan Mustafa Mohammed | IG: rowan_styles_" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 29.889748382633577, 31.205279161375767 ], [ 29.902391681418489, 31.205279161375767 ], [ 29.902437803216717, 31.204254259425593 ], [ 29.889778922002623, 31.204234322523305 ], [ 29.889748382633577, 31.205279161375767 ] ] ] ] } }
+]
+}
+;
